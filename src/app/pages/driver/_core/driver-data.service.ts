76123ecbd7980470ae5467fc66aa078172b90304
kjdeepak <kjdeepak4work@gmail.com>
2025-01-26 17:23:11 +0530

    Add environment file and update config

---
+++ src/app/pages/driver/_core/driver-data.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DriverDataService {
+
+  constructor() { }
+
+  fetchAllDrivers(): void {
+
+  }
+
+  fetchDriverById(id: string): void {
+
+  }
+
+  createDriver(driver: any): void {
+
+  }
+
+  updateDriver(driver: any): void {
+
+  }
+
+  deleteDriver(driver: any): void {
+
+  }
+}
